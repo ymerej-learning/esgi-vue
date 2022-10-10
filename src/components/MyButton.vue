d1c5e7c3353c5559df537541ae45754753ddc1d2
Jérémy Broyon <jeremy-broyon@outlook.fr>
2022-10-10 17:08:53 +0200

    add button for dark mode

---
+++ src/components/MyButton.vue
@@ -0,0 +1,51 @@
+<script setup>
+    const props = defineProps({
+        title: {
+            type: String,
+            required: true,
+        },
+        rounded: {
+            type: Boolean,
+            default: false,
+        },
+        background: {
+            type: String,
+            required: true,
+        },
+        color: {
+            type: String,
+            required: true,
+            default: "black",
+            validator(value) {
+                return ["black"].includes[value];
+            },
+        },
+        onClick: {
+            type: Function,
+            required: true,
+        },
+    });
+
+    function handleClick(event) {
+        console.info(event);
+        props.onClick();
+}
+</script>
+
+<template>
+    <button
+        v-bind:style="{
+            backgroundColor: background,
+            border: 'none',
+            borderRadius: rounded ? '0.5rem' : '0',
+            color: color,
+            cursor: 'pointer',
+        }"
+        v-on:click="handleClick($event)"
+    >
+        {{ title }}
+    </button>
+</template>
+
+<style scoped>
+</style>
